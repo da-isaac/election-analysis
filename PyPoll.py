de233ef3d8b2212d7b7cdff1a4c694251ef1c01e
da-isaac <ibjorgen97@gmail.com>
2022-05-16 20:19:38 -0600

    Added header and counties to new text file

--- PyPoll.py
+++ PyPoll.py
@@ -22,10 +22,25 @@ with open(file_to_load) as election_data:
 # Read data by indirect path to the file
 
 # Assign a variable to the file to load and the path
-file_to_load = os.path.join("Resources", "election_results.csv")
+##file_to_load = os.path.join("Resources", "election_results.csv")
 
 # Open the election results and read the file
-with open(file_to_load) as election_data:
+##with open(file_to_load) as election_data:
 
     #TODO: perform analysis
-    print(election_data)
+##    print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file
+file_to_save = 'analysis/election_analysis.txt'
+##file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file
+with open(file_to_save, "w") as txt_file:
+
+    # Add a header line and line separator
+    txt_file.write("Counties in the election\n")
+    txt_file.write("-------------------------\n")
+
+    # Write three counties to the file
+    txt_file.write("Arapahoe\nDenver\nJefferson")
+
