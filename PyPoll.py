7986886d9476f724692cfd9dd09b9830dfd72f28
da-isaac <ibjorgen97@gmail.com>
2022-05-16 21:26:13 -0600

    Added a list to store candidate names

--- PyPoll.py
+++ PyPoll.py
@@ -14,20 +14,36 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize total vote counter
+total_votes = 0
+
+# 2. Intialize candidate list
+candidate_options = []
+
 # Using the with statement open the file as a text file
 with open(file_to_load) as election_data:
-
-    # TODO: read and analyze data here
 
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
     # Print the header row
     headers = next(file_reader)
-    print(headers)
-    
 
     # Print each row in the CSV file
     for row in file_reader:
-        print(row)
-        #user print(row[0]) to print just first item for each row
+        # Add to the total vote count
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of the candidates
+            candidate_options.append(candidate_name)
+
+# Print the total votes
+print(total_votes)
+
+# Print the candidate list
+print(candidate_options)
