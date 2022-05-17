5d9b91ee4c0805c7911941b40dd2227d236d9ec4
da-isaac <ibjorgen97@gmail.com>
2022-05-16 20:33:28 -0600

    Added reader method to allow for analysis of election data

--- PyPoll.py
+++ PyPoll.py
@@ -8,39 +8,26 @@
 import csv
 import os
 
-# Read data by direct path to the file
-
-# Assign a variable for the file to load and the path
-file_to_load = 'Resources/election_results.csv'
-
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-
-    #TODO: perform analysis
-    print(election_data)
-
-# Read data by indirect path to the file
-
 # Assign a variable to the file to load and the path
-##file_to_load = os.path.join("Resources", "election_results.csv")
-
-# Open the election results and read the file
-##with open(file_to_load) as election_data:
-
-    #TODO: perform analysis
-##    print(election_data)
+file_to_load = os.path.join("Resources", "election_results.csv")
 
 # Create a filename variable to a direct or indirect path to the file
-file_to_save = 'analysis/election_analysis.txt'
-##file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Using the with statement open the file as a text file
-with open(file_to_save, "w") as txt_file:
+with open(file_to_load) as election_data:
 
-    # Add a header line and line separator
-    txt_file.write("Counties in the election\n")
-    txt_file.write("-------------------------\n")
+    # TODO: read and analyze data here
 
-    # Write three counties to the file
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+    # Read the file object with the reader function
+    file_reader = csv.reader(election_data)
 
+    # Print the header row
+    headers = next(file_reader)
+    print(headers)
+    
+
+    # Print each row in the CSV file
+    for row in file_reader:
+        print(row)
+        #user print(row[0]) to print just first item for each row
